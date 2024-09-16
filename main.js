e367828c2af4cef656a6e68d56ab8c4ea3d28a24
MihailFasteks <fastakovskiy.mihail04@gmail.com>
2024-09-17 02:21:30 +0300

    commited

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 var express = require('express');
-// создаем приложение, которое будет принимать запросы, обрабатывать их, и отправлять ответы
+
 var app = express();
 var path = require('path');
-// обработчик, который будет срабатывать на get запросы, для маршрута '/'
+const fs = require('fs');
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname,'pages/index.html')); 
 });
+app.post('/submit', function(req, res){ 
+    const email = req.body.eml;
+    const password = req.body.pswrd;
+    
+  
+    fs.appendFile('inf.txt', `Email: ${email}, Password: ${password}\n`, function(err) {
+        if (err) {
+            console.error('Ошибка записи в файл', err);
+            return res.status(500).send('Ошибка сервера');
+        }
+        
+      
+        res.redirect('/index.html');
+    });
+});
+
 app.get('/index.html', function(req, res){
     res.sendFile(path.join(__dirname,'pages/index.html')); 
 });
+
 app.get('/news.html', function(req, res){
     res.sendFile(path.join(__dirname,'pages/news.html')); 
 });
@@ -21,7 +40,19 @@ app.get('/register.html', function(req, res){
 app.get('/login.html', function(req, res){
     res.sendFile(path.join(__dirname,'pages/login.html')); 
 });
-
+app.get('/product/:productN/price/:price', function(request, response){
+var product=request.params['productN'];
+ var price=request.params['price'];
+    fs.appendFile('products.txt', `product: ${product}, price: ${price}\n`, function(err) {
+        if (err) {
+            console.error('Ошибка записи в файл', err);
+            return res.status(500).send('Ошибка сервера');
+        }
+        
+      
+        response.sendFile(path.join(__dirname,'pages/index.html')); 
+    });
+});
 app.listen(8080, function(){
     console.log('Server started on port: 8080');
 })
